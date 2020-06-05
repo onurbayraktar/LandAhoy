45629bb12d2b7d7c4072587c2756df7dea75ad27
onurbayraktar <obayraktar17@gmail.com>
2020-06-05 23:45:19 +0300

    Feat: Showing user location on google map.

--- app/src/main/java/com/example/onurb/landahoy/MainActivity.java
+++ app/src/main/java/com/example/onurb/landahoy/MainActivity.java
@@ -1,7 +1,18 @@
 package com.example.onurb.landahoy;
 
+import android.*;
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -13,6 +24,25 @@ import com.google.android.gms.maps.model.MarkerOptions;
 public class MainActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    LocationManager locationManager;
+    LocationListener locationListener;
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if(requestCode == 1)    // Request Code For FINE LOCATION //
+        {
+            if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+            {
+                if(checkForLocationPermission())
+                {
+                    locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+                }
+            }
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,22 +55,76 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
     }
 
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        locationListener = new LocationListener() {
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+            @Override
+            public void onLocationChanged(Location location) {
+                markUserLocationOnMap(location);
+            }
+
+            @Override
+            public void onStatusChanged(String s, int i, Bundle bundle) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String s) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String s) {
+
+            }
+        };
+
+        // Requesting Location Of User //
+        requestUserLocation();
     }
+
+
+    public void requestUserLocation()
+    {
+        if(Build.VERSION.SDK_INT < 23)  // If the SDK < 23; We Directly Request The Location Updates //
+        {
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+        }
+        else  // If the SDK >= 23; We Need To Check For Permission //
+        {
+            boolean permissionGranted = checkForLocationPermission();
+            if(!permissionGranted) // We Don't Have Permission //
+            {
+                ActivityCompat.requestPermissions
+                        (this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);  // Ask For Permission //
+            }
+            else  // We Have Permission //
+            {
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+                Location lastKnownLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                markUserLocationOnMap(lastKnownLocation);
+            }
+        }
+    }
+
+
+    public boolean checkForLocationPermission()
+    {
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED)
+        {
+            return false;
+        }
+        return true;
+    }
+
+    public void markUserLocationOnMap(Location location)
+    {
+        LatLng userLocation = new LatLng(location.getLatitude(), location.getLongitude());
+        mMap.addMarker(new MarkerOptions().position(userLocation).title("Your Location"));
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(userLocation, 18), 5000, null);
+    }
+
 }
